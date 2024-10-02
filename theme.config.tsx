b8551bc6f3c6022b84491b44725c9faf7da5180b
james-txt <jameslo.txt@gmail.com>
2024-10-02 13:21:46 -0400

    add app page

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
-import { Analytics } from '@vercel/analytics/react'
 
 const config: DocsThemeConfig = {
   logo: <span>Cybersecurity Awareness</span>,
@@ -22,15 +21,7 @@ const config: DocsThemeConfig = {
           </a>
       </span>
     )
-  },
-  head: ({ title, meta }: { title: string; meta: { description?: string } }) => (
-    <>
-      {meta.description && <meta name="description" content={meta.description} />}
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:title" content={title} />
-      <Analytics />
-    </>
-  ),
+  }
 }
 
 export default config
