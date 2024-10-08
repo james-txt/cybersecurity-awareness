a3a1f7d2a24f9b1b07ffd549935a165af72afd39
james-txt <jameslo.txt@gmail.com>
2024-10-07 22:55:01 -0400

    .

--- next.config.js
+++ next.config.js
@@ -3,9 +3,4 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.tsx',
 });
 
-module.exports = withNextra({
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.cache = false; // Comment this out or remove it to re-enable the cache
-    return config;
-  },
-});
+module.exports = withNextra({});
