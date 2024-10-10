013634ad8157e52d66db3a29e3d7b38303ffd49c
james-txt <jameslo.txt@gmail.com>
2024-10-10 17:32:39 -0400

    upd app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
-import '../globals.css'
+import '../globals.css';
+export const dynamic = 'force-dynamic';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
